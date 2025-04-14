850db56f67117e7ec7c7e90e7b366108ca545b2b
Shravya R S <rsshravya2@gmail.com>
2025-04-14 10:54:23 +0530

    Create test_mmcif_parser.py

---
+++ test_mmcif_parser.py
@@ -0,0 +1,10 @@
+import unittest
+from mmcif_parser import parse_mmcif
+
+class TestParser(unittest.TestCase):
+    def test_parse_headers(self):
+        result = parse_mmcif("example.cif")
+        self.assertIn("_entry.id", result)
+
+if __name__ == '__main__':
+    unittest.main()
